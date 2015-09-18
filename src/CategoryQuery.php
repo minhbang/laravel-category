38305be47a0a62b968ed982943cc60cc11bc7b91
Minh Bang <contact@minhbang.com>
2015-09-18 14:26:39 +0700

    add Category query trait

---
+++ src/CategoryQuery.php
@@ -0,0 +1,37 @@
+<?php
+namespace Minhbang\LaravelCategory;
+
+/**
+ * Class CategoryQuery
+ *
+ * @package Minhbang\LaravelCategory
+ * @property-read string $table
+ * @method \Illuminate\Database\Eloquent\Relations\BelongsTo belongsTo($related, $foreignKey = null, $otherKey = null, $relation = null)
+ */
+trait CategoryQuery
+{
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function category()
+    {
+        return $this->belongsTo(CategoryItem::class);
+    }
+
+    /**
+     * Tất cả content thuộc $category và con cháu của $category
+     *
+     * @param \Illuminate\Database\Query\Builder $query
+     * @param \Minhbang\LaravelCategory\CategoryItem $category
+     * @return \Illuminate\Database\Query\Builder
+     */
+    public function scopeCategorized($query, $category = null)
+    {
+        if (is_null($category)) {
+            return $query->with('category');
+        }
+        $ids = $category->getDescendantsAndSelf()->lists('id')->all();
+        return $query->with('category')
+            ->whereIn("{$this->table}.category_id", $ids);
+    }
+}
