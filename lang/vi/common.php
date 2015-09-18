9dad3fe8706478eb6d25d05d1c91a923ae2bdc7c
Minh Bang <contact@minhbang.com>
2015-09-18 09:36:12 +0700

    update translate

--- lang/vi/common.php
+++ lang/vi/common.php
@@ -5,7 +5,7 @@ return [
     'parent'         => 'Danh mục cha',
     'title'          => 'Tiêu đề',
     'slug'           => 'Tiêu đề SEO',
-    'item'           => 'Mục Danh con',
+    'item'           => 'Danh mục',
     'create_item'    => 'Thêm Danh mục con',
     'delete'         => 'Xóa Danh mục',
     'delete_confirm' => 'Bạn có chắc chắn muốn xóa Danh mục: {ITEM_NAME}<br><strong>Lưu ý:</trong> Tất cả mục Danh mục con của nó sẽ bị xóa cùng!',
