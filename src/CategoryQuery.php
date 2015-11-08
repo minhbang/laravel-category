66e06edc5738d7041b7581b83cb879293f981bbf
Minh Bang <contact@minhbang.com>
2015-11-08 10:24:53 +0700

    Sử dụng  descendantsAndSelf thay vì getDescendantsAndSelf

--- src/CategoryQuery.php
+++ src/CategoryQuery.php
@@ -30,7 +30,7 @@ trait CategoryQuery
         if (is_null($category)) {
             return $query->with('category');
         }
-        $ids = $category->getDescendantsAndSelf()->lists('id')->all();
+        $ids = $category->descendantsAndSelf()->lists('id')->all();
         return $query->with('category')
             ->whereIn("{$this->table}.category_id", $ids);
     }
