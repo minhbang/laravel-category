ee13a54d4fcca3aceba329b3c20b11d8f0531554
Minh Bang <contact@minhbang.com>
2016-03-10 10:40:46 +0700

    Refactor code

--- config/category.php
+++ config/category.php
@@ -2,7 +2,7 @@
 return [
     'types'         => ['article', 'ebook'],
     'default_type'  => 'article',
-    'presenter'     => Minhbang\Category\ItemPresenter::class,
+    'presenter'     => Minhbang\Category\CategoryPresenter::class,
     'add_route'     => true,
     'max_depth'     => 5,
     'middlewares'   => 'role:admin',
